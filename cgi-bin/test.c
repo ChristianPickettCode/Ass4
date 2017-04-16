bc8abc2492eddd2ec69b21e7a81f0544c1056808
Christian Pickett <christian.pickett@mail.mcgill.ca>
2017-04-15 21:02:08 -0400

    test

--- cgi-bin/test.c
+++ cgi-bin/test.c
@@ -17,7 +17,7 @@ void getValue(char newArr[], char array[], int index);
 
 void checkCMD(char *cmd, char *URL, int *mannaANDgold, Room room);
 void cmdDROP();
-void cmdPLAY();
+void cmdPLAY(char *cmd, char *URL);
 void cmdEXIT();
 void cmdREFRESH(char *data, char *URL, int *mannaANDgold);
 
@@ -100,7 +100,7 @@ void checkCMD(char *cmd, char *URL, int *mannaANDgold, Room room) {
 	}
 
 	else if (strcmp(cmd, "PLAY") == 0) {
-		cmdPLAY();
+		cmdPLAY(cmd, URL);
 	}
 
 	else if (strcmp(cmd, "EXIT") == 0) {
@@ -219,9 +219,45 @@ void cmdDROP(char *cmd, char *URL, int *mannaANDgold, Room room) {
 	}
 
 }
-void cmdPLAY() {
+void cmdPLAY(char *data, char *URL) {
+
+
 	printf("Content-Type:text/html\n\n");
-	printf("<p>PLAY</p>");
+
+	int activator = 0;
+    
+    if(strstr(data, "Larry") || strstr(data, "larry")) activator = 1;
+    else if(strstr(data, "QUIT") || strstr(data, "Quit")|| strstr(data, "quit")) activator = 3;
+    else activator = 2;
+
+    printf("Content-Type:text/html\n\n");
+    printf("<!DOCTYPE html>\n");
+    printf("<html>\n");
+    printf("<head>\n");
+    printf("<title>JIM/CHRIS/BEN</title>\n");
+    printf("<link rel='stylesheet' type='text/css' href='styles.css'>\n");
+    printf("</head>\n");
+    printf("<body bgcolor='white' text='black'>\n");
+    printf("<div class='fill-screen'>\n");
+    printf("<img class='make-it-fit' src='Dino.jpg' width=\"100%\" height=\"100%\">\n");
+    printf("<h3><span> Larry's Father had five kids. Their names were: Ten, Twenty, Thirty, and Forty. <span class='spacer'></span><br /><span class='spacer'></span> What was is fifth kids name? </span></h3>\n");
+    
+    if(activator == 1) printf("<h2><span> Congratulations! <span class='spacer'></span><br /><span class='spacer'></span> You answered correctly. </span></h2>\n");
+    if(activator == 2) printf("<h2><span> Incorrect. <span class='spacer'></span><br /><span class='spacer'></span> Please Try Again. </span></h2>\n");
+
+    printf("</div>\n");
+
+    printf("<center><form action='program.cgi' method='post'>\n");
+    printf("<input type='text' name='cmd' required class='search-box'>\n");
+    printf("<submits><input type='submit' class='button' value='Submit'></submits>\n");
+    printf("</form></center>\n");
+
+    printf("</body>\n");
+    printf("</html>\n");
+    
+    if(activator == 1 || activator == 3) printf("<meta http-equiv='refresh' content='5; url=http://cs.mcgill.ca/~bkinse/cgi-bin/room.html'>");
+
+
 }
 
 void cmdEXIT() {
